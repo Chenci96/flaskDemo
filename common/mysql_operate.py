3ac385ffc36c2f3d8ee7ec79e39ae75689033f6a
cc <chenc@hrtps.com>
2022-01-13 14:24:11 +0800

    test_api第一次提交

---
+++ common/mysql_operate.py
@@ -0,0 +1,56 @@
+import pymysql
+from config.setting import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWD, MYSQL_DB
+
+class MysqlDb():
+
+    def __init__(self, host, port, user, passwd, db):
+        # 建立数据库连接
+        self.conn = pymysql.connect(
+            host=host,
+            port=port,
+            user=user,
+            passwd=passwd,
+            db=db,
+            charset='utf8',
+            autocommit=True,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        # 通过 cursor() 创建游标对象，并让查询结果以字典格式输出
+        self.cur = self.conn.cursor()
+
+    def __del__(self):  # 对象资源被释放时触发，在对象即将被删除时的最后操作
+        # 关闭游标
+        self.cur.close()
+        # 关闭数据库连接
+        self.conn.close()
+
+    def select_db(self, sql):
+        """查询"""
+        # 检查连接是否断开，如果断开就进行重连
+        self.conn.ping(reconnect=True)
+        # 使用 execute() 执行sql
+        self.cur.execute(sql)
+        # 使用 fetchall() 获取查询结果
+        data = self.cur.fetchall()
+        return data
+
+    def execute_db(self, sql):
+        """更新/新增/删除"""
+        try:
+            # 检查连接是否断开，如果断开就进行重连
+            self.conn.ping(reconnect=True)
+            # 使用 execute() 执行sql
+            self.cur.execute(sql)
+            # 提交事务
+            self.conn.commit()
+        except Exception as e:
+            print("操作出现错误：{}".format(e))
+            # 回滚所有更改
+            self.conn.rollback()
+
+db = MysqlDb(MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWD, MYSQL_DB)
+
+if __name__ == '__main__':
+
+    sql = "SELECT * FROM user"
+    print(db.select_db(sql))
